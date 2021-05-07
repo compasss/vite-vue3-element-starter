d1411ddfc3cbe290e0fba6a5e8f49d46ad1ce3f3
postree <crazymingzai@gmail.com>
2021-05-07 18:19:45 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { components, plugins } from './ui'
+
+// import ElementPlus from 'element-plus'
+// import 'element-plus/packages/theme-chalk/src/base.scss'
+// import 'element-plus/lib/theme-chalk/index.css'
+// createApp(App).use(ElementPlus).mount('#app')
+
+const app = createApp(App)
+components.forEach(component => {
+  app.component(component.name, component)
+})
+
+plugins.forEach(plugin => {
+  app.use(plugin)
+})
+
+app.mount('#app')
